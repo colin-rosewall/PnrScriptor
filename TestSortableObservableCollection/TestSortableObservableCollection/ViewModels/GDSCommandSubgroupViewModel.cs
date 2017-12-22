0d09568ccfdd24d197ebeac8212c165f401a73bd
colin-rosewall <colin.rosewall@flightcentre.com.au>
2017-12-22 13:42:47 +1000

    Validate Subgroup Description.

--- TestSortableObservableCollection/TestSortableObservableCollection/ViewModels/GDSCommandSubgroupViewModel.cs
+++ TestSortableObservableCollection/TestSortableObservableCollection/ViewModels/GDSCommandSubgroupViewModel.cs
@@ -1,27 +1,34 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using TestSortableObservableCollection.Interfaces;
 using TestSortableObservableCollection.ViewModels;
 
 namespace TestSortableObservableCollection.ViewModels
 {
-    public class GDSCommandSubgroupViewModel : Base.BaseViewModel, IGDSCommandSubgroupViewModel
+    public class GDSCommandSubgroupViewModel : Base.BaseViewModel, IGDSCommandSubgroupViewModel, INotifyDataErrorInfo
     {
         private string _description = null;
         private IGDSCommandItemViewModel _parent = null;
         private SortableObservableCollection<IGDSCommandItemViewModel> _children = null;
         private bool _IsItemExpanded = false;
         private bool _IsItemSelected = false;
+        private Dictionary<string, List<string>> _validationErrors = null;
 
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
         public GDSCommandSubgroupViewModel(IGDSCommandItemViewModel parent, string theDescription)
         {
             _parent = parent;
             _description = theDescription;
             _children = new SortableObservableCollection<IGDSCommandItemViewModel>();
+            _validationErrors = new Dictionary<string, List<string>>();
         }
 
         public SortableObservableCollection<IGDSCommandItemViewModel> Children
@@ -43,7 +50,8 @@ namespace TestSortableObservableCollection.ViewModels
             {
                 if (_description != value)
                 {
-                    _description = value;
+                   _description = value;
+                    ValidateDescription(_description, () => Description);
                     NotifyPropertyChanged(() => Description);
                 }
             }
@@ -91,6 +99,14 @@ namespace TestSortableObservableCollection.ViewModels
             }
         }
 
+        public bool HasErrors
+        {
+            get
+            {
+                return _validationErrors.Any();
+            }
+        }
+
         public void AddChildItem(IGDSCommandItemViewModel item)
         {
             _children.Add(item);
@@ -99,6 +115,75 @@ namespace TestSortableObservableCollection.ViewModels
         public object Clone()
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            List<string> errorMessages = new List<string>();
+            if (propertyName != null)
+            {
+                _validationErrors.TryGetValue(propertyName, out errorMessages);
+                return errorMessages;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void RaiseErrorsChanged(string propertyName)
+        {
+            if (ErrorsChanged != null)
+                ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        private void ValidateDescription(string newValue, Expression<Func<string>> propName)
+        {
+            const string descriptionMissing = "Subgroup Description cannot be empty.";
+
+            var lambda = (LambdaExpression)propName;
+            MemberExpression memberExpression;
+            string memberName = null;
+
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+            memberName = memberExpression.Member.Name;
+
+            newValue = newValue.Trim();
+            if (string.IsNullOrEmpty(newValue))
+            {
+                if (_validationErrors.ContainsKey(memberName))
+                {
+                    List<string> existingMessages = null;
+                    if (_validationErrors.TryGetValue(memberName, out existingMessages))
+                    {
+                        if (existingMessages != null)
+                        {
+                            if (!existingMessages.Exists(msg => msg.Equals(descriptionMissing)))
+                            {
+                                _validationErrors[memberName].Add(descriptionMissing);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    _validationErrors.Add(memberName, new List<string> { descriptionMissing });
+                    RaiseErrorsChanged(memberName);
+                }
+            }
+            else if (_validationErrors.ContainsKey(memberName))
+            {
+                _validationErrors.Remove(memberName);
+                RaiseErrorsChanged(memberName);
+            }
         }
     }
 }
