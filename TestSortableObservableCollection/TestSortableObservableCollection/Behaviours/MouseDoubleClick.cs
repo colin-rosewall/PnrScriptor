791b20c6e1fc344df354dc13a5f7844158720362
colin-rosewall <colin.rosewall@flightcentre.com.au>
2018-01-24 22:28:52 +1000

    Add MouseDoubleClick attached property behaviour.

---
+++ TestSortableObservableCollection/TestSortableObservableCollection/Behaviours/MouseDoubleClick.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TestSortableObservableCollection.Behaviours
+{
+    public static class MouseDoubleClick
+    {
+        public static DependencyProperty CommandProperty =
+            DependencyProperty.RegisterAttached(
+            "Command",
+            typeof(ICommand),
+            typeof(MouseDoubleClick),
+            new UIPropertyMetadata(OnCommandChanged));
+
+        public static DependencyProperty CommandParameterProperty =
+            DependencyProperty.RegisterAttached(
+            "CommandParameter",
+            typeof(object),
+            typeof(MouseDoubleClick),
+            new UIPropertyMetadata(null));
+
+        public static ICommand GetCommand(DependencyObject target)
+        {
+            return (ICommand)target.GetValue(CommandProperty);
+        }
+
+        public static void SetCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(CommandProperty, value);
+        }
+
+        public static object GetCommandParameter(DependencyObject target)
+        {
+            return target.GetValue(CommandParameterProperty);
+        }
+
+        public static void SetCommandParameter(DependencyObject target, object value)
+        {
+            target.SetValue(CommandParameterProperty, value);
+        }
+
+        private static void OnCommandChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            Control control = target as Control;
+            if (control != null)
+            {
+                if ((e.NewValue != null) && (e.OldValue == null))
+                {
+                    control.MouseDoubleClick += OnMouseDoubleClick;
+                }
+                else if ((e.NewValue == null) && (e.OldValue != null))
+                {
+                    control.MouseDoubleClick -= OnMouseDoubleClick;
+                }
+            }
+        }
+
+        private static void OnMouseDoubleClick(object sender, RoutedEventArgs e)
+        {
+            Control control = sender as Control;
+
+            if (control != null)
+            {
+                ICommand command = (ICommand)control.GetValue(CommandProperty);
+                object commandParameter = control.GetValue(CommandParameterProperty);
+                if (command != null)
+                {
+                    command.Execute(commandParameter);
+                    e.Handled = true;
+                }     
+            }
+        }
+    }
+}
