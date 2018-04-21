4c887efa4e9647f8a714b982c6f3407e3e0684b9
colin-rosewall <colin.rosewall@flightcentre.com.au>
2018-04-21 23:33:45 +1000

    Experiment with regular expression matching.

--- TestSortableObservableCollection/TestSortableObservableCollection/Views/ScriptGenerationWindow.xaml.cs
+++ TestSortableObservableCollection/TestSortableObservableCollection/Views/ScriptGenerationWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using TestSortableObservableCollection.ViewModels;
+using System.Text.RegularExpressions;
 
 namespace TestSortableObservableCollection.Views
 {
@@ -26,6 +27,31 @@ namespace TestSortableObservableCollection.Views
             InitializeComponent();
 
             DataContext = vm;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as ScriptGenerationViewModel;
+
+            if (vm != null)
+            {
+                string maskRegEx = @"([a-zA-Z\d\/ -]+\*\*)([\da-zA-Z ]*[.\d-]*)";
+                Regex test1 = new Regex(maskRegEx);
+                if (test1.IsMatch(vm.ScriptInput))
+                {
+                    MatchCollection mc = test1.Matches(vm.ScriptInput);
+                    foreach (Match m in mc)
+                    {
+                        
+                        GroupCollection groups = m.Groups;
+                        if (groups.Count > 2)
+                        {
+                            MessageBox.Show(string.Format("{0}:{1}", groups[1].Value, groups[2].Value));
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
