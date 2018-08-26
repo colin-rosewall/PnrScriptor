24cde48c8360cd2ffd5d98cd4a826d37e6541b06
colin-rosewall <colin.rosewall@flightcentre.com.au>
2018-08-26 20:00:29 +1000

    Refactor Replacements code so we can add unit tests.

---
+++ TestSortableObservableCollection/TestSortableObservableCollection/Helpers/ReplacementsHelper.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pidgin;
+using static Pidgin.Parser;
+
+namespace TestSortableObservableCollection.Helpers
+{
+    static class ReplacementsHelper
+    {
+        public static bool ReplaceAmaAvail(ref string lineCopy, ref int availabilityCounter, IEnumerable<Models.Flight> flights)
+        {
+            bool result = false;
+
+            var transactionCodeParser = from gap1 in Parser.SkipWhitespaces
+                                  from firstTransactionCode in Parser.CIChar('a')
+                                  from secondTransactionCode in OneOf(Try(Parser.CIChar('n')), Try(Parser.CIChar('d')), Try(Parser.CIChar('a')))
+                                  select new string(new char[] { firstTransactionCode, secondTransactionCode }).ToUpper();
+
+            var OptionalDayDigitParser = from secondDigit in Parser.Digit.Optional()
+                                   select (secondDigit.HasValue ? new string(new char[] { secondDigit.Value }) : string.Empty);
+
+            var dayPartParser = from gap1 in Parser.SkipWhitespaces
+                          from firstDigit in Parser.Digit
+                          from secondOptionalDigit in OptionalDayDigitParser
+                          select new string(new char[] { firstDigit }) + secondOptionalDigit;
+
+            var monthPartParser = from gap1 in Parser.SkipWhitespaces
+                            from monthShortName in Parser.OneOf(
+                                Try(Parser.CIString("JAN")), Try(Parser.CIString("FEB")), Try(Parser.CIString("MAR")), Try(Parser.CIString("APR")),
+                                Try(Parser.CIString("MAY")), Try(Parser.CIString("JUN")), Try(Parser.CIString("JUL")), Try(Parser.CIString("AUG")),
+                                Try(Parser.CIString("SEP")), Try(Parser.CIString("OCT")), Try(Parser.CIString("NOV")), Try(Parser.CIString("DEC")))
+                            select monthShortName.ToUpper();
+
+            var cityPartParser = from gap1 in Parser.SkipWhitespaces
+                           from city in Parser.Letter.Repeat(3)
+                           select new string(city.ToArray()).ToUpper();
+
+            var AvailabilityParser = from transactionCode in transactionCodeParser
+                        from dayPart in dayPartParser
+                        from monthPart in monthPartParser
+                        from origin in cityPartParser
+                        from destination in cityPartParser
+                        select new { transactionCode, dayPart, monthPart, origin, destination };
+
+            var ParserResult = AvailabilityParser.Parse(lineCopy);
+            if (ParserResult.Success)
+            {
+                var flt = flights.ElementAtOrDefault(availabilityCounter);
+                if (flt != null)
+                {
+                    var tc = ParserResult.Value.transactionCode;
+                    var dt = string.IsNullOrEmpty(flt.TravelDate) ? ParserResult.Value.dayPart + ParserResult.Value.monthPart : flt.TravelDate;
+                    var or = string.IsNullOrEmpty(flt.Origin) ? ParserResult.Value.origin : flt.Origin;
+                    var ds = string.IsNullOrEmpty(flt.Destination) ? ParserResult.Value.destination : flt.Destination;
+                    lineCopy = string.Concat(tc,  dt,  or,  ds);
+                }
+                availabilityCounter++;
+                result = true;
+            }
+
+            return result;
+        }
+
+    }
+}
--- TestSortableObservableCollection/TestSortableObservableCollection/ViewModels/ScriptGenerationViewModel.cs
+++ TestSortableObservableCollection/TestSortableObservableCollection/ViewModels/ScriptGenerationViewModel.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using TestSortableObservableCollection.Models;
 using TestSortableObservableCollection.ViewModels.Base;
+using TestSortableObservableCollection.Helpers;
 
 namespace TestSortableObservableCollection.ViewModels
 {
@@ -78,80 +79,25 @@ namespace TestSortableObservableCollection.ViewModels
         public void ApplyReplacements_Executed(object obj)
         {
             StringBuilder sb = new StringBuilder();
-            int availabilityCount = 0;
+            int availabilityCounter = 0;
 
             string[] lines = _scriptInput.Split(new [] { Environment.NewLine, "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string line in lines)
+            foreach (string lineOfInput in lines)
             {
-                string aLine = line;
+                string lineCopy = lineOfInput;
                 bool replacementsMade = false;
 
-                replacementsMade = ReplaceAmaAvail(ref aLine, ref availabilityCount);
+                replacementsMade = ReplacementsHelper.ReplaceAmaAvail(ref lineCopy, ref availabilityCounter, _flights);
                 if (!replacementsMade)
                 {
-                    sb.AppendLine(line);
+                    sb.AppendLine(lineOfInput);
                 }
                 else
                 {
-                    sb.AppendLine(aLine);
+                    sb.AppendLine(lineCopy);
                 }
             }
             ScriptOutput = sb.ToString();
-        }
-
-        private bool ReplaceAmaAvail(ref string line, ref int availabilityCount)
-        {
-            bool result = false;
-
-            var transactionCode = from gap1 in Parser.SkipWhitespaces
-                                  from firstTransactionCode in Parser.CIChar('a')
-                                  from secondTransactionCode in OneOf(Try(Parser.CIChar('n')), Try(Parser.CIChar('d')), Try(Parser.CIChar('a')))
-                                  select new string(new char[] { firstTransactionCode, secondTransactionCode }).ToUpper();
-
-            var OptionalDayDigit = from secondDigit in Parser.Digit.Optional()
-                                   select (secondDigit.HasValue ? new string(new char[] { secondDigit.Value }) : string.Empty);
-
-            var dayPart = from gap1 in Parser.SkipWhitespaces
-                          from firstDigit in Parser.Digit
-                          from secondOptionalDigit in OptionalDayDigit
-                          select new string(new char[] { firstDigit }) + secondOptionalDigit;
-
-            var monthPart = from gap1 in Parser.SkipWhitespaces
-                            from monthShortName in Parser.OneOf(
-                                Try(Parser.CIString("JAN")), Try(Parser.CIString("FEB")), Try(Parser.CIString("MAR")), Try(Parser.CIString("APR")),
-                                Try(Parser.CIString("MAY")), Try(Parser.CIString("JUN")), Try(Parser.CIString("JUL")), Try(Parser.CIString("AUG")),
-                                Try(Parser.CIString("SEP")), Try(Parser.CIString("OCT")), Try(Parser.CIString("NOV")), Try(Parser.CIString("DEC")))
-                            select monthShortName.ToUpper();
-
-            var cityPart = from gap1 in Parser.SkipWhitespaces
-                           from city in Parser.Letter.Repeat(3)
-                           select new string(city.ToArray()).ToUpper();
-
-            var test1 = from a in transactionCode
-                        from b in dayPart
-                        from c in monthPart
-                        from origin in cityPart
-                        from destination in cityPart
-                        select new { a, b, c, origin, destination };
-
-            var presult = test1.Parse(line);
-            if (presult.Success)
-            {
-                var flt = _flights.ElementAtOrDefault(availabilityCount);
-                if (flt != null)
-                {
-                    var tc = presult.Value.a;
-                    var dt = string.IsNullOrEmpty(flt.TravelDate) ? presult.Value.b + presult.Value.c : flt.TravelDate;  
-                    var or = string.IsNullOrEmpty(flt.Origin) ? presult.Value.origin : flt.Origin;
-                    var ds = string.IsNullOrEmpty(flt.Destination) ? presult.Value.destination : flt.Destination;
-                    line = string.Concat(tc," ", dt, " ", or, " ", ds);
-                }
-                availabilityCount++;
-                result = true;
-            }
-                
-
-            return result;
         }
 
         public bool ApplyReplacements_CanExecute(object obj)
